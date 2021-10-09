706f86e9da275e0f8de9f47a66718b5d26c0fcb2
vanna2020 <vannasingh1999@gmail.com>
2021-10-09 10:33:01 -0700

    Welcome message

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+    public class MoodAnalyzer {
+        public static void main(String[] args){
+            System.out.println("welcome to Mood Analyzer Problem");
+        }
+    }
+
+
